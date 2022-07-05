f425aba4e02d42afb8b1e2cb72352ec016780494
Tuhana GAYRETLİ <bentuhana+github@proton.me>
2022-07-05 16:53:06 +0300

    fix stupid mess

--- env.ts
+++ env.ts
@@ -4,9 +4,6 @@ export const GIT_BRANCH = JSON.stringify(
   execSync('git branch --show-current').toString().trim()
 );
 export const GIT_COMMIT_HASH = JSON.stringify(
-  execSync(`git rev-parse ${GIT_BRANCH !== 'main' ? GIT_BRANCH : 'HEAD'}`)
-    .toString()
-    .trim()
-    .slice(0, 7)
+  execSync('git rev-parse HEAD').toString().trim().slice(0, 7)
 );
 export const REPO_URL = 'https://github.com/bentuhana/website';
